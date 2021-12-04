b50c5b708fac4a7417eac4ccc394cafe21ba4cea
Axultan <araykhanov@ozon.ru>
2021-12-05 00:44:43 +0300

    refactored

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -21,8 +21,8 @@ func Unpack(inStr string) (string, error) {
 
 	for idx, r := range inStrRuned {
 		var (
-			runeToAdd       = r
-			mult      int64 = 1
+			runeToAdd = r
+			mult      = 1
 		)
 		switch {
 		case unicode.IsDigit(r):
@@ -32,7 +32,7 @@ func Unpack(inStr string) (string, error) {
 			default:
 				if idx > 0 && !multiplied {
 					multiplied = true
-					mult = int64(r-'0') - 1
+					mult = int(r-'0') - 1
 					runeToAdd = resultRune[len(resultRune)-1]
 				} else {
 					return "", fmt.Errorf("digit at [0] position in string or digits in a row without \\ err: %w", ErrInvalidString)
@@ -60,14 +60,15 @@ func Unpack(inStr string) (string, error) {
 	return string(resultRune), nil
 }
 
-func add(target []rune, runeToCopy rune, n int64) []rune {
+func add(target []rune, runeToCopy rune, n int) []rune {
 	switch {
 	case n < 0:
 		target = target[:len(target)-1]
-	case n >= 0:
-		for i := 0; i < int(n); i++ {
+	case n > 0:
+		for i := 0; i < n; i++ {
 			target = append(target, runeToCopy)
 		}
+	default:
 	}
 	return target
 }
