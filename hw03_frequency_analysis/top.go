54bc5175e9bb4d69f1250a5308cae724530177fa
Axultan <araykhanov@ozon.ru>
2021-12-06 01:27:48 +0300

    tests passed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(inText string) []string {
+	const (
+		cutSet       = `,.!@#$%^&*()-"'[]\/+=|`
+		regNonCutSet = `[^-\p{L}]+`
+	)
+	var (
+		words           = strings.Fields(inText)
+		wordToFrequency = make(map[string]int64)
+		arrangedWords   []string
+		reg             = regexp.MustCompile(regNonCutSet)
+		lessFunc        = func(i, j int) bool {
+			if wordToFrequency[arrangedWords[i]] == wordToFrequency[arrangedWords[j]] {
+				return arrangedWords[i] < arrangedWords[j]
+			}
+			return wordToFrequency[arrangedWords[i]] > wordToFrequency[arrangedWords[j]]
+		}
+	)
+
+	for _, word := range words {
+		word = strings.ToLower(strings.Trim(word, cutSet))
+		if len(word) < 1 {
+			continue
+		}
+		word = reg.ReplaceAllString(word, "")
+
+		if _, ok := wordToFrequency[word]; !ok {
+			wordToFrequency[word] = 1
+			arrangedWords = append(arrangedWords, word)
+			sort.Slice(arrangedWords, lessFunc)
+			continue
+		}
+		wordToFrequency[word]++
+		sort.Slice(arrangedWords, lessFunc)
+	}
+	if len(arrangedWords) < 10 {
+		return arrangedWords
+	}
+	return arrangedWords[:10]
 }
