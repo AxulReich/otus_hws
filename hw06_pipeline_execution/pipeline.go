2c9d274a61cce6560461e68e408097be171c4e26
Axultan <araykhanov@ozon.ru>
2022-03-14 01:03:06 +0300

    some changes

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,23 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+
+	go func() {
+		var (
+			wg sync.WaitGroup
+		)
+		for _, stage := range stages {
+			wg.Add(1)
+			stage := stage
+			go func() {
+				out := stage(in)
+				in = outStage
+			}()
+
+		}
+
+		wg.Wait()
+	}()
+
+	return out
 }
