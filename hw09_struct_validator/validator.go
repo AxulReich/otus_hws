c897c56a188beff04bd34f9a60cd9f559512db41
Raykhanov Aksultan Serikovich <araykhanov@ozon.ru>
2022-04-11 02:48:41 +0300

    st

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,10 @@
 package hw09structvalidator
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +13,21 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	s := strings.Builder{}
+
+	for _, e := range v {
+		s.WriteString(fmt.Sprintf("field: %q - %s\n", e.Field, e.Err.Error()))
+	}
+
+	return s.String()
 }
 
 func Validate(v interface{}) error {
+	if ref
+	switch v.(type) {
+	case :
+
+	}
 	// Place your code here.
 	return nil
 }
