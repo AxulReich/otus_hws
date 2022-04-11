87d6e670026fc49f7bf3ded196a3f28c92969da9
Raykhanov Aksultan Serikovich <araykhanov@ozon.ru>
2022-04-12 00:08:36 +0300

    refactored

--- hw10_program_optimization/stats_optimization_test.go
+++ hw10_program_optimization/stats_optimization_test.go
@@ -58,28 +58,6 @@ func TestGetDomainStat_Time_And_Memory(t *testing.T) {
 	require.Less(t, mem, memoryLimit, "the program is too greedy")
 }
 
-//func BenchmarkGetDomainStatOptimised(b *testing.B) {
-//	r, _ := zip.OpenReader("testdata/users.dat.zip")
-//	defer r.Close()
-//
-//	data, _ := r.File[0].Open()
-//
-//	for i := 0; i < b.N; i++ {
-//		_, _ := GetDomainStatOptimised(data, "biz")
-//	}
-//}
-//
-//func BenchmarkGetDomainStat(b *testing.B) {
-//	r, _ := zip.OpenReader("testdata/users.dat.zip")
-//	defer r.Close()
-//
-//	data, _ := r.File[0].Open()
-//
-//	for i := 0; i < b.N; i++ {
-//		_, _ := GetDomainStat(data, "biz")
-//	}
-//}
-
 var expectedBizStat = DomainStat{
 	"abata.biz":         25,
 	"abatz.biz":         25,
